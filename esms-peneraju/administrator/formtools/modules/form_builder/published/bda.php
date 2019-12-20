1748dfc8eea086e905e689cfeddf981ff98ab6b9
Abdul Alim <alim@genaxis.com.my>
2019-12-20 16:39:10 +0800

    Auto stash before merge of "master" and "origin/master"

---
+++ esms-peneraju/administrator/formtools/modules/form_builder/published/bda.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * This file was created by the Form Tools Form Builder module.
+ */
+require_once('C:\wamp64\www\e-SMS\esms-peneraju\administrator\formtools/global/library.php');
+use FormTools\Core;
+Core::init(array("auto_logout" => false));
+$root_dir = Core::getRootDir();
+$published_form_id = 3;
+$filename  = "bda.php";
+require_once("$root_dir/modules/form_builder/form.php");
